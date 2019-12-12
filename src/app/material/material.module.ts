cfb9be96a9699a3dbc7a1d158bf1d1adb8b51098
srinivas-ang <57973930+srinivas-ang@users.noreply.github.com>
2019-12-12 21:08:28 +0530

    add material paginator and sort modules

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {
-  MatButtonModule, MatCheckboxModule,
-  MatFormFieldModule, MatInputModule,MatAutocompleteModule,
+  MatButtonModule, MatCheckboxModule,MatPaginatorModule,MatSortModule,
+  MatFormFieldModule, MatInputModule,MatAutocompleteModule,MatTooltipModule,
   MatTableModule, MatToolbarModule, MatIconModule, MatTabsModule, MatExpansionModule, MatGridListModule, MatCardModule, MatRadioModule
   , MatSelectModule, MatDatepickerModule, MatNativeDateModule, MatProgressSpinnerModule, MatSnackBarModule, MatDialogModule, MatMenuModule,
   MatDividerModule, MatSlideToggleModule, MatListModule
@@ -12,7 +12,7 @@ import  {MatProgressButtonsModule} from 'mat-progress-buttons';
 import { NgxMatSelectSearchModule } from 'ngx-mat-select-search';
 const modules = [
   MatButtonModule, MatCheckboxModule,MatAutocompleteModule,NgxMatSelectSearchModule,
-  MatFormFieldModule, MatInputModule,
+  MatFormFieldModule, MatInputModule,MatPaginatorModule,MatSortModule,MatTooltipModule,
   MatTableModule, MatToolbarModule, MatIconModule, MatTabsModule, MatExpansionModule, MatGridListModule, MatCardModule, MatRadioModule
   , MatSelectModule, MatDatepickerModule, MatNativeDateModule, MatProgressSpinnerModule, MatSnackBarModule, MatDialogModule, MatMenuModule,
   MatDividerModule, MatSlideToggleModule, MatListModule,MatProgressButtonsModule
