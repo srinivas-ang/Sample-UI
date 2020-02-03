d1f6baabef9bd914758fab708e9309caba028745
srinivas-ang <57973930+srinivas-ang@users.noreply.github.com>
2020-02-03 23:01:49 +0530

    search client service call changes

--- src/app/services/addclient.service.ts
+++ src/app/services/addclient.service.ts
@@ -9,8 +9,8 @@ export class AddclientService {
   baseUrl="http://wdvra97a0184.wellsfargo.com:9003/";
   constructor(private _http:HttpClient) { }
 
-  searchSalesforceClients(data):Observable<any>{
-    return this._http.get<any>(this.baseUrl+'getSalesforceClients')
-      //  return this._http.get('api/client?searchtext=' + data)
+  searchSalesforceClients(criteriaType, criteriaValue):Observable<any>{
+    // return this._http.get<any>(this.baseUrl+'getSalesforceClients')
+       return this._http.get('api/searchClient/'+criteriaType + '/' + criteriaValue)
   }
 }
