21c74f551184be8bd3b8650aa1b9460e6b035d31
srinivas-ang <57973930+srinivas-ang@users.noreply.github.com>
2020-02-08 00:44:33 +0530

    new change

--- src/app/services/addclient.service.ts
+++ src/app/services/addclient.service.ts
@@ -1,16 +1,25 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AddclientService {
-  baseUrl="http://wdvra97a0184.wellsfargo.com:9003/";
+  headers:HttpHeaders;
+   baseUrl="http://localhost:3200/";
   constructor(private _http:HttpClient) { }
 
   searchSalesforceClients(criteriaType, criteriaValue):Observable<any>{
-    // return this._http.get<any>(this.baseUrl+'getSalesforceClients')
-       return this._http.get('api/searchClient/'+criteriaType + '/' + criteriaValue)
+    var userDetails=JSON.parse(localStorage.getItem("loginUserDetails"));
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'appName': 'IBCM',
+        'username':userDetails ? userDetails.UserName:'sample'
+      })
+    };
+    return this._http.get<any>(this.baseUrl+'getSalesforceClients',httpOptions);
+    // return this._http.get('api/searchClient/'+ criteriaType + '/' + criteriaValue,httpOptions);
   }
 }
