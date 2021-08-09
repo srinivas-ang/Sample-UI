7e750bd3d616e8d9614fc5a2ad2c8528e001d3f2
srinivas-ang <57973930+srinivas-ang@users.noreply.github.com>
2021-08-09 19:25:36 +0530

    recycle bin search deails

--- src/app/components/recyclebin/recyclebin.component.ts
+++ src/app/components/recyclebin/recyclebin.component.ts
@@ -1,5 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { SelectionModel } from '@angular/cdk/collections';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 import { MatProgressButtonOptions } from 'mat-progress-buttons';
 import { Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
@@ -20,6 +24,32 @@ export class RecyclebinComponent implements OnInit {
   lstUsers:any=[];
   protected _onDestroy = new Subject<void>();
   isSubmitted:boolean=false;
+
+  showSearchData:boolean=false;
+  displayedColumns = ['select','objectName', 'deletedDate','deletedBy','objectPath']
+  lastSearchData: MatTableDataSource<any>;
+  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
+  @ViewChild(MatSort, { static: true }) sort: MatSort;
+  selection = new SelectionModel<any>(true, []);
+  data:any=[{
+    objectName:'Srinivas',
+    deletedDate:"10/03/2021",
+    deletedBy:'Srinivas',
+    objectPath:'wef/dffdf/dsfdsfsd/dsfdsfdfd'
+  },
+  {
+    objectName:'Srinivas',
+    deletedDate:"10/03/2021",
+    deletedBy:'Srinivas',
+    objectPath:'wef/dffdf/dsfdsfsd/dsfdsfdfd'
+  },
+  {
+    objectName:'Srinivas',
+    deletedDate:"10/03/2021",
+    deletedBy:'Srinivas',
+    objectPath:'wef/dffdf/dsfdsfsd/dsfdsfdfd'
+  }]
+
   pitchSpinnerButtonOptions: MatProgressButtonOptions = {
     active: false,
     text: 'Search',
@@ -67,13 +97,51 @@ export class RecyclebinComponent implements OnInit {
   getUsers(){
   }
   applySearch(){
+    this.showSearchData=true;
     debugger;
     if (this._recyclebinForm.invalid) {
       this.isSubmitted = true;
       this._recyclebinForm.markAllAsTouched();
       return;
     }
+    debugger
+    var obj={
+      objectName:this.f.objectName.value,
+      folderPath:this.f.filePath.value,
+      objectType:this.f.folder.value,
+      date:this.f.dateRange.value
+
+    }
+
+    this.lastSearchData = new MatTableDataSource(this.data);
+      this.lastSearchData.paginator = this.paginator;
+      this.lastSearchData.sort = this.sort;
+    console.log(obj)
     console.log(this._recyclebinForm)
 
+  }
+  applyFilter(filterValue: string) {
+    filterValue = filterValue.trim(); // Remove whitespace
+    this.lastSearchData.filter = filterValue;
+  }
+
+   /** Whether the number of selected elements matches the total number of rows. */
+   isAllSelected() {
+    const numSelected = this.selection.selected.length;
+    const numRows = this.lastSearchData.data.length;
+    return numSelected === numRows;
+  }
+
+  /** Selects all rows if they are not all selected; otherwise clear selection. */
+  masterToggle() {
+    this.isAllSelected() ?
+        this.selection.clear() :
+        this.lastSearchData.data.forEach(row => this.selection.select(row));
+  }
+
+  logSelection() {
+    debugger
+    console.log(this.selection.selected)
+    this.selection.selected.forEach(s => console.log(s.objectName));
   }
 }
